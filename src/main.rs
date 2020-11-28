598d93aad11150e552132bf4415ba532012a4a22
Austin Mullins <austinmullins9@gmail.com>
2020-11-28 16:50:21 -0500

    [Core] Added libs. Cleaned up main. There are few commit message issues this push.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 mod hashing;
+mod inbound;
+mod dispatcher;
+mod log;
 
-fn main() {
-    println!("Tangent Core");
+fn print_logo() {
+    println!(r"
+    _________  ________  ________   ________  _______   ________   _________   
+   |\___   ___\\   __  \|\   ___  \|\   ____\|\  ___ \ |\   ___  \|\___   ___\ 
+   \|___ \  \_\ \  \|\  \ \  \\ \  \ \  \___|\ \   __/|\ \  \\ \  \|___ \  \_| 
+        \ \  \ \ \   __  \ \  \\ \  \ \  \  __\ \  \_|/_\ \  \\ \  \   \ \  \  
+         \ \  \ \ \  \ \  \ \  \\ \  \ \  \|\  \ \  \_|\ \ \  \\ \  \   \ \  \ 
+          \ \__\ \ \__\ \__\ \__\\ \__\ \_______\ \_______\ \__\\ \__\   \ \__\
+           \|__|  \|__|\|__|\|__| \|__|\|_______|\|_______|\|__| \|__|    \|__| 
+    ");
+}
+
+#[tokio::main]
+async fn main() {
+    /* Display the tangent logo on run. */
+    print_logo();
+
+    /* When we get a job system going, we will have no need for this. */
+    tokio::spawn(async move {
+        let _ = match inbound::reciever::start_inbound_server().await {
+            Ok(n) => n,
+            Err(e) => log::print_error("Reciever", &format!("The inbound reciever failed to intilize: {:?}", e))
+        };
+    });
+
+    log::print_error("Core", "Reached");
 }
