07c3d601a6d8e248ff75ec8c8f18f10d83b88755
Austin Mullins <austinmullins9@gmail.com>
2020-11-28 16:48:01 -0500

    [Dispatcher] Used tokio to handle a simpleTCP listen connection

---
+++ src/dispatcher.rs
@@ -0,0 +1,11 @@
+/*
+*
+*	Author: Austin Mullins
+*	Copyright: Tangent
+*
+*
+*   This file will host the job system for our application.
+*   The reason it'll be neccesarry is because of this "module"
+*   based data analyzer idea we've came up with. We want to be 
+*   able to have multiple modules working in parallel. 
+*/
