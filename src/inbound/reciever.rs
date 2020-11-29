1636a635a8240eab016647c6814218b9bb380dd7
Austin Mullins <austinmullins9@gmail.com>
2020-11-28 19:45:09 -0500

    [Reciever] Added a file output for a recieved data package.

--- src/inbound/reciever.rs
+++ src/inbound/reciever.rs
@@ -8,6 +8,8 @@
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 use super::super::log::*;
+use super::super::fs;
+use super::super::hashing;
 
 static INBOUND_PORT: &str = "7777";
 
@@ -23,7 +25,6 @@ pub async fn start_inbound_server() -> Result<(), Box<dyn std::error::Error>> {
 
             loop {
                 let n = match socket.read(&mut buffer).await {
-                    // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => n,
                     Err(e) => {
@@ -32,7 +33,12 @@ pub async fn start_inbound_server() -> Result<(), Box<dyn std::error::Error>> {
                     }
                 };
 
-                print_normal("Reciever", &format!("[Reviever] Retrived: {:?}", n));
+                let filename = &hashing::sha256::generate_sha256_hash(&format!("{:?}", std::time::Instant::now()));
+                match fs::write_file(filename, fs::OutputType::Inbound, &buffer[0..n]) {
+                    Ok(_) => print_normal("FileSystem", &format!("File: {:?} created successfully.", filename)),
+                    Err(e) => print_error("FileSystem", &format!("There was an issue making the inbound file: {:?} \n {:?}", filename, e))
+                }
+                print_normal("Reciever", &format!("[Reviever] Retrived: {:?}", String::from_utf8(buffer[0..n].to_vec()).unwrap()));
             }
         });
     }
