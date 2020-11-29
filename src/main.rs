65bf5e608c23365df776770b7c789dbbd2f4ee8a
Austin Mullins <austinmullins9@gmail.com>
2020-11-28 19:46:13 -0500

    [Core] Misc work.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod hashing;
 mod inbound;
 mod dispatcher;
+mod fs;
 mod log;
 
 fn print_logo() {
@@ -20,6 +21,16 @@ async fn main() {
     /* Display the tangent logo on run. */
     print_logo();
 
+    /* Create eh folders we will be using */
+    match fs::initilize_fs() {
+        Ok(_) => {},
+        Err(e) => {
+            if e.kind() != std::io::ErrorKind::AlreadyExists {
+                log::print_error("FileSystem", &format!{"There was an issue creating the folders for the file system: {:?}", e})
+            } else { log::print_normal("FileSystem", "There already exists folders for the file system.")}
+        }
+    };
+
     /* When we get a job system going, we will have no need for this. */
     tokio::spawn(async move {
         let _ = match inbound::reciever::start_inbound_server().await {
@@ -28,5 +39,8 @@ async fn main() {
         };
     });
 
-    log::print_error("Core", "Reached");
+    loop {
+        // if ALL JOBS FINISHED
+        std::thread::sleep(std::time::Duration::from_millis(1))
+    }
 }
