86ba87c1fa950fd8e893e9c4506001b92eead8e9
Austin Mullins <austinmullins9@gmail.com>
2020-12-08 22:15:06 -0500

    [Filesystem] Changed the write_file function to dictated opening first.

--- src/fs.rs
+++ src/fs.rs
@@ -5,6 +5,9 @@
 *
 */
 
+use super::log::*;
+use std::io::Write;
+
 static INBOUND_FILE_PATH: &str = "tmp/data/inbound/";
 
 pub enum OutputType {
@@ -14,18 +17,31 @@ pub enum OutputType {
 /* Goes through and makes sure each of the paths we are going to be using are instantiated. */
 pub fn initilize_fs() -> Result<(), std::io::Error>{
     std::fs::create_dir("tmp/")?;
-    std::fs::create_dir("tmp/data/");
-    std::fs::create_dir("tmp/data/inbound/");
+    std::fs::create_dir("tmp/data/")?;
+    std::fs::create_dir("tmp/data/inbound/")?;
 
     Ok(())
 }
 
 pub fn write_file(filename: &str, o_type: OutputType, data: &[u8]) -> Result<(), std::io::Error>{
+    let filepath: String = [INBOUND_FILE_PATH, filename, ".tan"].join("");
+
+    let mut file: std::fs::File = match std::fs::OpenOptions::new().read(true).open(&filepath) {
+        Ok(f) => f,
+        Err(_) => {
+            match std::fs::File::create(&filepath) {
+                Ok(f) => { print_normal("FileSystem", &format!("File: {:?} created successfully.", filename)); f }
+                Err(e) => return Err(e)
+            }
+        },
+    };
+
+
+
     match o_type {
         OutputType::Inbound => {
-            std::fs::write([INBOUND_FILE_PATH, filename, ".tan"].join(""), data)?;
+            file.write(data)?;
         }
-        _ => {}
     }
 
     Ok(())
